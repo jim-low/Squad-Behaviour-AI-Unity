a714179e54f8ea870518d1a33234a50a12d3b907
ZullyStunts <96515529+ZullyStunts@users.noreply.github.com>
2023-05-07 21:18:54 +0800

    moved some files and added GUI

---
+++ Assets/Scripts/Utilities/redManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class redManager : MonoBehaviour
+{
+    private GameObject[] allReds;
+
+    //public void ReduceHP() {
+    //    allReds = GameObject.FindGameObjectsWithTag("Ally");
+
+    //    foreach (GameObject red in allReds) 
+    //    {
+    //        red.GetComponent<Soldier>().;
+    //        (Soldier)red.health -= ((Soldier)red.MAX_HEALTH * 0.1);
+    //        (Soldier)red.SetHealth(health);
+    //    }
+            
+    //}
+
+    //public void HealToFull() { 
+    
+    //}
+}
