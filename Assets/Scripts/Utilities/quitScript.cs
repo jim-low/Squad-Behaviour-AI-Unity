efc283fe3c71666b6dfd41b1fe9889351a424c6f
ZullyStunts <96515529+ZullyStunts@users.noreply.github.com>
2023-05-07 23:35:26 +0800

    made a quit button

---
+++ Assets/Scripts/Utilities/quitScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quitScript : MonoBehaviour
+{
+    public void quitGame()
+    { 
+        Application.Quit();
+    }
+}
